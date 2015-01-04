49b76c503221f3eb8b3a49c554fab7b7ff96166e
panicbit <panicbit@users.noreply.github.com>
2015-01-04 04:51:05 +0100

    Simplify broadcast implementation

--- src/broadcast.rs
+++ src/broadcast.rs
@@ -1,9 +1,8 @@
-use std::collections::DList;
 use std::sync::{Mutex,Arc};
 
 #[deriving(Clone)]
 pub struct BroadcastStation<T: Send> {
-    clients: Arc<Mutex<DList<Sender<T>>>>
+    clients: Arc<Mutex<Vec<Sender<T>>>>
 }
 
 unsafe impl<T: Send> Sync for BroadcastStation<T> {}
@@ -11,34 +10,27 @@ unsafe impl<T: Send> Sync for BroadcastStation<T> {}
 impl<T: Send+Clone> BroadcastStation<T> {
     pub fn new() -> BroadcastStation<T> {
         BroadcastStation::<T> {
-            clients: Arc::new(Mutex::new(DList::new()))
+            clients: Arc::new(Mutex::new(Vec::new()))
         }
     }
     
     pub fn signup(&mut self) -> Receiver<T> {
         let (cast_tx, cast_rx) = channel();
         let mut clients = self.clients.lock().unwrap();
-        clients.push_back(cast_tx);
+        clients.push(cast_tx);
         cast_rx
     }
 
     pub fn send(&mut self, msg: T) {
-        // This implementation is not satisfactory.
-        // I'd rather avoid "client.clone()" and
-        // just move the clients into the new list.
         let mut clients = self.clients.lock().unwrap();
-        *clients = clients.iter().flat_map(|client| {
-            client
-                .send_opt(msg.clone())
-                .ok()
-                .map(move |_| client.clone())
-                .into_iter()
-        }).collect();
+        clients.retain(|client|
+            client.send(msg.clone()).is_ok()
+        );
     }
 
     #[allow(dead_code)]
     pub fn disconnect_all(&mut self) {
         let mut clients = self.clients.lock().unwrap();
-        *clients = DList::new();
+        *clients = Vec::new();
     }
 }
